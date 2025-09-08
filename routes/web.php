d2a297fe24a419d7846238d4bfb594642c4f6313
corydorasssterbai <chocoogyuu@gmail.com>
2025-09-08 22:43:41 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProyekController;
-
-
+use App\Http\Controllers\ProyekInvoiceController;
+use App\Http\Controllers\ProyekKwitansiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('customer', CustomerController::class);
 Route::resource('proyek', ProyekController::class);
+Route::resource('proyek_invoice', ProyekInvoiceController::class);
+Route::resource('proyek_kwitansi', ProyekKwitansiController::class);
